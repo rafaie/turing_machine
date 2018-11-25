5417ba5efeae366e9f37105eae24eee183a1db28
Mostafa <mostafa.rafaie@gmail.com>
2018-11-25 02:26:57 -0600

    Complete the implementation

--- turing.py
+++ turing.py
@@ -8,8 +8,30 @@ import argparse
 
 
 MAX_TRANSITION = 20
-START_STATE_NAME='q0'
-END_STATE_NAME='qr'
+START_STATE_NAME = 'q0'
+ACCEPT_STATE_NAME = 'qa'
+REJECT_STATE_NAME = 'qr'
+
+EMPTY_CHR = '_'
+
+START = 1
+REJECT = 2
+LOOP = 3
+UNKNOWN = 5
+ACCEPT = 10
+
+
+class Transition:
+    def __init__(self, state1, symbol1, state2, symbol2, move):
+        self.symbol1 = symbol1
+        self.symbol2 = symbol2
+        self.state1 = state1
+        self.state2 = state2
+        self.move = move
+
+    def show(self):
+        print('\t', self.state1.name, ',', self.symbol1, '->',
+              self.state2.name, ',', self.symbol2, ',', self.move)
 
 
 class State:
@@ -18,12 +40,24 @@ class State:
         self.transitions = []
 
     def add_transition(self, symbol1, state2, symbol2, move):
-        self.transitions.append([symbol1, state2, symbol2, move])
+        self.transitions.append(Transition(self, symbol1,
+                                           state2, symbol2, move))
 
     def show(self):
         print('state name: ', self.name)
         for t in self.transitions:
-            print('\t', t[0], '->', t[1].name, ',', t[2], ',', t[3])
+            t.show()
+
+    @property
+    def st_type(self):
+        if self.name == START_STATE_NAME:
+            return START
+        elif self.name == REJECT_STATE_NAME:
+            return REJECT
+        elif self.name == ACCEPT_STATE_NAME:
+            return ACCEPT
+
+        return UNKNOWN
 
 
 class Turing:
@@ -43,7 +77,7 @@ class Turing:
         l = init_string.split('#')
         if len(l) <= 2:
             return None
-
+        self.machine_str = '#'.join(l[:-2])
         for i in range(1, len(l)-3):
             str_list = l[i].split(',')
             st1 = self.get_state(str_list[0])
@@ -55,19 +89,92 @@ class Turing:
 
         return l[-2]
 
-    def show(self):
-        print(self.init_string)
-        print(self.str)
+    def show(self, str=None):
+        print(self.machine_str)
+        print(str if str is not None else self.str)
+        print(sum([len(self.states[id].transitions) for id in self.states]))
+        print(len(self.states))
+
         if self.verbose is True:
             print("State details:")
             for id in self.states:
                 self.states[id].show()
 
+    def show_iteration(self, it, st=None):
+        print(st, it[0].name, it[1], it[2], it[3])
+
     def run(self, str=None):
         if str is None:
             str = self.str
 
-        pass
+        self.show(str)
+
+        it = [self.states[START_STATE_NAME], str, 0, UNKNOWN]
+        it_repo = [[it]]
+        it_counter = 0
+        it_status = UNKNOWN
+
+        while (it_status is not ACCEPT) and it_counter <= self.max_transitions:
+            if self.verbose is True:
+                print('------------------------')
+                print("it_counter:", it_counter)
+            it_status = UNKNOWN
+            it_repo.append([])
+            for it in it_repo[it_counter]:
+                if self.verbose is True:
+                    print('------------------------')
+                    self.show_iteration(it, 'Start State')
+
+                if it[3] == UNKNOWN:
+                    state = it[0]
+                    s = it[1]
+                    i = it[2]
+                    if i >= len(s):
+                        c = EMPTY_CHR
+                    else:
+                        c = s[i]
+
+                    for tr in state.transitions:
+                        if tr.symbol1 == '_' or tr.symbol1 == c:
+                            # print('++++++++++++++++++')
+                            # tr.show()
+                            str_new = s
+                            if tr.symbol2 != '_':
+                                if i < 0:
+                                    # recheck the case
+                                    pass
+                                elif i >= len(str_new):
+                                    # print("i >= len(str_new)", i, len(str_new))
+                                    str_new += '_' * (i-len(str_new)) \
+                                        + tr.symbol2
+                                else:
+                                    # print("else", i, len(str_new))
+                                    str_new = str_new[:i] + tr.symbol2 + \
+                                        str_new[i+1:]
+
+                            # print("str_new", str_new)
+
+                            if tr.move == 'R':
+                                i_new = i + 1
+                            elif tr.move == 'L':
+                                i_new = 1 - 1
+                            # print(tr.move, i, i_new)
+
+                            it_repo[it_counter+1].append([tr.state2, str_new,
+                                                          i_new,
+                                                          tr.state2.st_type])
+                            if self.verbose is True:
+                                self.show_iteration(it_repo[it_counter+1][-1],
+                                                    '\t')
+
+            it_counter += 1
+            it_status = max([it[-1] for it in it_repo[it_counter]])
+
+        if it_status == ACCEPT:
+            print('M stops and accepts w')
+        else:
+            print('M stops and reject w')
+
 
 # example
 # init_string = #q0,a->qa,a,R#q0,a->q1,a,R#q0,b->q1,b,R#q0,_->q1,_,R#q1,b->q1,b,R#q1,_->q1,_,R#q1,a->qr,a,R#q1,b->qr,b,R##ab#
@@ -87,5 +194,4 @@ if __name__ == "__main__":
         MAX_TRANSITION
 
     t = Turing(args.machine, max_transitions, args.verbose)
-    t.show()
     t.run()
