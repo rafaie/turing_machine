f787829d934e68e9eed853507a159ae64a27e702
Mostafa <mostafa.rafaie@gmail.com>
2018-11-25 03:05:22 -0600

    Remove comments!

---
+++ run_tests.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "---------------------------------------------------"
+echo "python3 turing.py -f testcase1.txt"
+python3 turing.py -f testcase1.txt
+
+echo "---------------------------------------------------"
+echo "python3 turing.py -f testcase2.txt"
+python3 turing.py -f testcase2.txt
+
+echo "---------------------------------------------------"
+echo "python3 turing.py -f testcase3.txt"
+python3 turing.py -f testcase3.txt
+
+echo "---------------------------------------------------"
+echo "python3 turing.py -f testcase4.txt"
+python3 turing.py -f testcase4.txt
+
+echo "---------------------------------------------------"
+echo "python3 turing.py -f testcase5.txt"
+python3 turing.py -f testcase5.txt
+
+echo "---------------------------------------------------"
+echo "python3 turing.py -f testcase6.txt"
+python3 turing.py -f testcase6.txt
